a392b1de44d49ca952489d3100ae5f786e0fdf72
Gabriel Jones <gabe1122k@gmail.com>
2023-01-06 20:17:34 -0600

    Added the prework files into the repository

---
+++ IntroductionStudent/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  IntroductionStudent
+//
+//  Created by Gabe Jones on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var yearSegementedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBAction func introduceSelfDidTap(_ sender: UIButton) {
+        let year = yearSegementedControl.titleForSegment(at: yearSegementedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) pets. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
